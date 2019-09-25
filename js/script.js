fc967cb3d49c955fe484bab70960f736bf1a8343
christopherbergstrom <christopherrbergstrom@gmail.com>
2019-09-24 21:38:34 -0700

    added foundation

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
 $(document).ready(() => {
     // $("body").css("background-color", "black");
     let games;
-    $.get("games.json", (data) => {
+    $.get("gameList.json", (data) => {
         games = data;
         // console.log(data[0].name);
     });
-    $("input").keyup((e) => {
+    $("#searchBox").keyup((e) => {
         // console.log($("input").val());
-        games.forEach((game) => {
-            if ($("input").val() === game.name) {
-                // console.log(game.name);
-                $("body").append(`<div class="w3-row-padding w3-margin-top">
-                    <div class="w3-third">${game.name}</div>
-                    <div class="w3-third">test2</div>
-                    <div class="w3-third">test3</div>
-                </div>`);
-
-            }
-        });
+        console.log(e.which);
+        if (e.which == 13) {
+            console.log("here");
+            games.forEach((game) => {
+                if ($("input").val() === game.name) {
+                    // console.log(game.name);
+                    $("header").after(`<div class="grid-x grid-margin-x">
+                        <div class="cell medium-4 text-center">${game.name}</div>
+                        <div class="cell medium-4 text-center">test2</div>
+                        <div class="cell medium-4 text-center">test3</div>
+                    </div>`);
+                }
+            });
+        }
     });
 });
