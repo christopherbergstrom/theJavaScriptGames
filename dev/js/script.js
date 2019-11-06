84ffff7e421b727af2bfa5e107aac03d7ead2dca
christopherbergstrom <christopherrbergstrom@gmail.com>
2019-11-05 21:38:16 -0800

    added sass and grid, took out foundation

---
+++ dev/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(() => {
+  // $("body").css("background-color", "black");
+  let games;
+  $.get("gameList.json", (data) => {
+      games = data;
+      // console.log(data[0].name);
+  });
+  $("#searchBox").keyup((e) => {
+      // console.log($("input").val());
+      console.log(e.which);
+      if (e.which == 13) {
+          console.log("here");
+          games.forEach((game) => {
+              if ($("input").val() === game.name) {
+                  // console.log(game.name);
+                  $("header").after(`<div class="grid-x grid-margin-x">
+                      <div class="cell medium-4 text-center">${game.name}</div>
+                      <div class="cell medium-4 text-center">test2</div>
+                      <div class="cell medium-4 text-center">test3</div>
+                  </div>`);
+              }
+          });
+      }
+  });
+});
